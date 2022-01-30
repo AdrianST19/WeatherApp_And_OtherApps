8e8061cc7e1943add0633db9ec2471edff9b7170
Adrian Stefan <92085719+AdrianST19@users.noreply.github.com>
2022-01-30 03:13:39 +0200

    Update NameAge.py

--- NameAge.py
+++ NameAge.py
@@ -1,22 +1,39 @@
-# Predict age based on a name
-
+from urllib import request
 import requests
 
-BASE_URL="https://api.agify.io/"
+name = input("Input your name: ")
 
-name=input("Input your name: ")
-request_url= f"{BASE_URL}?name={name}"
+# Name age
+BASE_URL = "https://api.agify.io/"
+request_url = f"{BASE_URL}?name={name}"
 response = requests.get(request_url)
 
-if response.status_code == 200:
-    data = response.json()
-    name=data['name']
-    age=data['age']
-    checkLetters=False
-    for i in range(len(name)):
-        if name[i].upper()<'A' or name[i].upper()>'Z': checkLetters=True
+# Name origin
+BASE_URL2 = "https://api.nationalize.io/?name="
+request_url2 = f"{BASE_URL2}{name}"
+response2 = requests.get(request_url2)
 
-    print("You have to introduce a name...with letters!") if checkLetters==True else print(f"Your name is {name} and you are {age} years old!")
-    
+if response.status_code == 200 and response2.status_code == 200:
+    # If everything is ok, it prints the age based on the name
+    data = response.json()
+    name = data['name']
+    age = data['age']
+    checkLetters = False
+    for i in range(len(name)):
+        if name[i].upper() < 'A' or name[i].upper() > 'Z':
+            checkLetters = True
+
+    print("You have to introduce a name...with letters!") if checkLetters == True else print(
+        f"Your name is {name} and you are {age} years old!")
+
+    # If everything is ok, it prints the origin of the name
+    data2 = response2.json()
+    print(f"The origins of the name {name} are:")
+    i = 0
+    while i < len(data2['country']):
+        print("Country: ", data2['country'][i]['country_id'])
+        print("Probability: ", round(
+            data2['country'][i]['probability'], 2), "%")
+        i += 1
 else:
-    print("An error occurred.")
+    print("Something went wrong, try again!")
